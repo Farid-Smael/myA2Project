db9c3d9a038b6ef13796695c1114a66d24349517
Check <farid.m.check@gmail.com>
2017-04-17 22:21:15 +0200

    Remove ngDoCheck and _save function. Add onSubmit function to call OrderService features + Validators.compose.

--- src/app/order-form/order-form.component.ts
+++ src/app/order-form/order-form.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {OrderService} from "../order.service";
 
 declare var $: any;
 @Component({
@@ -8,25 +9,32 @@ declare var $: any;
   styleUrls: ['./order-form.component.scss']
 })
 export class OrderFormComponent implements OnInit {
-	form: FormGroup;
+	f: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-	this.form = this.fb.group({
-		order_num: '',
-		costumer: '',
-		product : '',
-		quantity : '',
-		price : ''
-	});
+	constructor(private fb: FormBuilder, private _os: OrderService) {
+		this.f = this.fb.group({
+			order_num: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(6)])],
+			costumer: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(20)])],
+			product: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(15)])],
+			quantity: ['', Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(2)])],
+			price: ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(6)])]
+		});
+	}
 
-  }
+	ngOnInit() {
+	}
 
-  ngOnInit() {
-  }
+	onSubmit(f: FormGroup) {
 
-  onSubmit(f: FormGroup){
-  	console.log(f);
-  }
+		// console.log(f.get('order_num').value);
+		this._os.addOrder({
+			'order_num': f.get('order_num').value,
+			'costumer': f.get('costumer').value,
+			'product': f.get('product').value,
+			'quantity': f.get('quantity').value,
+			'price': f.get('price').value
+		});
+	}
 
 	@ViewChild('order_num') order_num: ElementRef;
 	@ViewChild('costumer') costumer: ElementRef;
@@ -35,15 +43,5 @@ export class OrderFormComponent implements OnInit {
 	@ViewChild('price') price: ElementRef;
 
 
-	ngDoCheck() {
-		if (this.order_num.nativeElement.classList.contains('open')) {
-			$(this).addClass('field-non-valid');
-		}
-	}
-
-	private _save(data: FormGroup) {
-		console.log(data);
-
-	}
 
 }
